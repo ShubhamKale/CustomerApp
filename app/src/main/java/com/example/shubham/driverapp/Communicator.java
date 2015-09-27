4633aa431c1fd39013aa2a76f3f92f13c7c59373
ShubhamKale <kaleshubham46@gmail.com>
2015-09-27 15:26:29 +0530

    MyFirstProject

---
+++ app/src/main/java/com/example/shubham/driverapp/Communicator.java
@@ -0,0 +1,8 @@
+package com.example.shubham.driverapp;
+
+/**
+ * Created by shubham on 26/9/15.
+ */
+public interface Communicator {
+    public void loginToken(String str);
+}
---
+++ app/src/main/java/com/example/shubham/driverapp/MainPanel.java
@@ -0,0 +1,17 @@
+package com.example.shubham.driverapp;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class MainPanel extends Fragment{
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View v=inflater.inflate(R.layout.fragment_main_panel,container,false);
+        return v;
+    }
+}
